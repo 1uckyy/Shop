b446f4d94fee5e3c125a58da65408101d0dd4c0e
Vladislav Novak <v.novak@labmedia.su>
2022-02-28 16:33:01 +0300

    Start of implementing Vue

---
+++ Shop.UI/wwwroot/js/admin/main.js
@@ -0,0 +1,20 @@
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        price: 0,
+        showPrice: true
+    },
+    computed: {
+        formatPrice: function () {
+            return "$" + this.price;
+        }
+    },
+    methods: {
+        togglePrice: function () {
+            this.showPrice = !this.showPrice;
+        },
+        log(v) {
+            console.log(v);
+        }
+    }
+});
