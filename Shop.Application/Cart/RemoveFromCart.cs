2072dd7149afbd62f071fa780863c261440c4072
Vladislav Novak <novak.vlad97@mail.ru>
2022-05-19 23:52:26 +0300

    Updating Cart (In process)

---
+++ Shop.Application/Cart/RemoveFromCart.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Shop.Database;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Cart
+{
+    public class RemoveFromCart
+    {
+        private ISession _session;
+        private ApplicationDbContext _ctx;
+
+        public RemoveFromCart(ISession session, ApplicationDbContext ctx)
+        {
+            _session = session;
+            _ctx = ctx;
+        }
+
+        public async Task<bool> Do(Request request)
+        {
+            var cartList = new List<CartProduct>();
+            var stringObject = _session.GetString("cart");
+
+            if (!string.IsNullOrEmpty(stringObject))
+            {
+                return true;
+            }
+            cartList = JsonConvert.DeserializeObject<List<CartProduct>>(stringObject);
+
+            if (!cartList.Any(x => x.StockId == request.StockId))
+            {
+                return true;
+            }
+
+            cartList.Find(x => x.StockId == request.StockId).Qty -= request.Qty;
+
+            stringObject = JsonConvert.SerializeObject(cartList);
+
+            _session.SetString("cart", stringObject);
+
+            var stockOnHold = _ctx.StockOnHolds
+                .FirstOrDefault(x => x.StockId == request.StockId
+                            && x.SessionId == _session.Id);
+
+            stockOnHold.Qty -= request.Qty;
+
+            if (stockOnHold.Qty <= 0)
+            {
+                _ctx.Remove(stockOnHold);
+            }
+
+            await _ctx.SaveChangesAsync();
+
+            return true;
+        }
+
+        public class Request
+        {
+            public int StockId { get; set; }
+            public int Qty { get; set; }
+        }
+    }
+}
---
+++ Shop.UI/Controllers/CartController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Application.Cart;
+using Shop.Database;
+using System.Threading.Tasks;
+
+namespace Shop.UI.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class CartController : Controller
+    {
+        private ApplicationDbContext _ctx;
+
+        public CartController(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpPost("{stockId}")]
+        public async Task<IActionResult> AddOne(int stockId)
+        {
+            var request = new AddToCart.Request
+            {
+                StockId = stockId,
+                Qty = 1
+            };
+
+            var addToCart = new AddToCart(HttpContext.Session, _ctx);
+
+            var success = await addToCart.Do(request);
+
+            if (success)
+            {
+                return Ok("Item Added to cart");
+            }
+
+            return BadRequest("Failed to add to cart");
+        }
+
+        [HttpPost("{stockId}")]
+        public async Task<IActionResult> RemoveOne(int stockId)
+        {
+            var request = new RemoveFromCart.Request
+            {
+                StockId = stockId,
+                Qty = 1
+            };
+
+            var addToCart = new RemoveFromCart(HttpContext.Session, _ctx);
+
+            var success = await addToCart.Do(request);
+
+            if (success)
+            {
+                return Ok("Item Added to cart");
+            }
+
+            return BadRequest("Failed to add to cart");
+        }
+    }
+}
